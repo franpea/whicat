ab33db2bc49c1157414175e2366142699e3ca540
chrrrrrris <chris.p.chan94@gmail.com>
2017-05-29 19:47:02 +1000

    update not working, unsure of error source

---
+++ update.py
@@ -0,0 +1,10 @@
+from app import db
+import os.path
+from models import QuestionSet
+
+def updateQuestion(number, newQuestion):
+    question = db.session.query(QuestionSet).filter_by(QuestionNumber=number)
+    question.QuestionText = newQuestion
+    db.session.commit()
+
+updateQuestion(1, "What is a duck")
