941364d78275393eafccd69b224c5eeced52cd4b
Fran Peaston <fran.peaston@icloud.com>
2017-05-29 19:54:31 +1000

    this is my second attempt with new home & checklist pages

--- app/views.py
+++ app/views.py
@@ -5,6 +5,55 @@ from .forms import LoginForm
 @app.route('/')
 @app.route('/index')
 def index():
+    navigation = [  # navigation menu items
+        {
+            'href': '/index', 
+            'caption': 'Home' 
+        },
+        { 
+            'href': '/checklist', 
+            'caption': 'Checklist' 
+        },
+        { 
+            'href': '/tutorial', 
+            'caption': 'Tutorial' 
+        },
+        { 
+            'href': '/login', 
+            'caption': 'Login' 
+        }
+    ]
+    return render_template('index.html',
+                           title='Home',
+						   navigation=navigation)
+
+@app.route('/checklist')
+def checklist():
+    navigation = [  # navigation menu items
+        {
+            'href': '/index', 
+            'caption': 'Home' 
+        },
+        { 
+            'href': '/checklist', 
+            'caption': 'Checklist' 
+        },
+        { 
+            'href': '/tutorial', 
+            'caption': 'Tutorial' 
+        },
+        { 
+            'href': '/login', 
+            'caption': 'Login' 
+        }
+    ]
+    return render_template('checklist.html',
+                           title='Checklist',
+						   navigation=navigation)
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
     user = {'nickname': 'Miguel'}  # fake user
     posts = [  # fake array of posts
         { 
@@ -16,14 +65,8 @@ def index():
             'body': 'The Avengers movie was so cool!' 
         }
     ]
-    return render_template("index.html",
-                           title='Home',
-                           user=user,
-                           posts=posts)
-
-@app.route('/login', methods=['GET', 'POST'])
-def login():
     form = LoginForm()
     return render_template('login.html', 
                            title='Sign In',
-                           form=form)
+                           form=form,
+						   user=user)
