75cae277aff4a04c36b7a5dde9da9a45e28e54f7
chrrrrrris <chris.p.chan94@gmail.com>
2017-06-04 14:08:58 +1000

    backend updates

--- app/forms.py
+++ app/forms.py
@@ -1,7 +1,11 @@
 from flask_wtf import Form
-from wtforms import StringField, BooleanField
+from wtforms import Form, TextField, TextAreaField, validators, StringField, SubmitField, BooleanField
 from wtforms.validators import DataRequired
+
 
 class LoginForm(Form):
     openid = StringField('openid', validators=[DataRequired()])
     remember_me = BooleanField('remember_me', default=False)
+
+class UrlForm(Form):
+    url = StringField('URL:', validators=[validators.required()])
--- app/views.py
+++ app/views.py
@@ -1,6 +1,18 @@
-from flask import render_template
+import os
+from flask import Flask, request, session, g, redirect, url_for, abort, \
+     render_template, flash
 from app import app
-from .forms import LoginForm
+from .forms import LoginForm, UrlForm
+
+
+
+
+def reduceURL(url):
+    if(url[4] == 's'):
+        return url[8:]
+    else:
+        return url[7:]
+
 
 @app.route('/')
 @app.route('/index')
@@ -27,8 +39,14 @@ def index():
                            title='Home',
 						   navigation=navigation)
 
-@app.route('/checklist')
+@app.route('/checklist', methods=['GET', 'POST'])
 def checklist():
+    form = UrlForm()
+    if request.method == 'POST':
+        name = request.form['URL']
+        print name
+        session['name'] = name
+        return redirect(url_for('question1'))
     navigation = [  # navigation menu items
         {
             'href': '/index', 
@@ -49,7 +67,8 @@ def checklist():
     ]
     return render_template('checklist.html',
                            title='Checklist',
-						   navigation=navigation)
+						   navigation=navigation,
+                          form=form)
 
 
 @app.route('/login', methods=['GET', 'POST'])
@@ -70,3 +89,77 @@ def login():
                            title='Sign In',
                            form=form,
 						   user=user)
+
+@app.route('/checklist/question/1', methods=['GET', 'POST'])
+def question1():
+    name = session['name']
+    return render_template('checklist-Q1.html', name=name, title=reduceURL(name))
+
+@app.route('/checklist/question/2', methods=['GET', 'POST'])
+def question2():
+    name = session['name']
+    return render_template('checklist-Q2.html', name=name, title=reduceURL(name))
+
+
+@app.route('/checklist/question/3', methods=['GET', 'POST'])
+def question3():
+    name = session['name']
+    return render_template('checklist-Q3.html', name=name, title=reduceURL(name))
+
+
+@app.route('/checklist/question/4', methods=['GET', 'POST'])
+def question4():
+    name = session['name']
+    return render_template('checklist-Q4.html', name=name, title=reduceURL(name))
+
+
+@app.route('/checklist/question/5', methods=['GET', 'POST'])
+def question5():
+    name = session['name']
+    return render_template('checklist-Q5.html', name=name, title=reduceURL(name))
+
+
+@app.route('/checklist/question/6', methods=['GET', 'POST'])
+def question6():
+    name = session['name']
+    return render_template('checklist-Q6.html', name=name, title=reduceURL(name))
+
+
+@app.route('/checklist/question/7', methods=['GET', 'POST'])
+def question7():
+    name = session['name']
+    return render_template('checklist-Q7.html', name=name, title=reduceURL(name))
+
+
+@app.route('/checklist/question/8', methods=['GET', 'POST'])
+def question8():
+    name = session['name']
+    return render_template('checklist-Q8.html', name=name, title=reduceURL(name))
+
+
+@app.route('/checklist/question/9', methods=['GET', 'POST'])
+def question9():
+    name = session['name']
+    return render_template('checklist-Q9.html', name=name, title=reduceURL(name))
+
+
+@app.route('/checklist/question/10', methods=['GET', 'POST'])
+def question10():
+    name = session['name']
+    return render_template('checklist-Q10.html', name=name, title=reduceURL(name))
+
+
+@app.route('/checklist/question/11', methods=['GET', 'POST'])
+def question11():
+    name = session['name']
+    return render_template('checklist-Q11.html', name=name, title=reduceURL(name))
+
+
+@app.route('/checklist/question/12', methods=['GET', 'POST'])
+def question12():
+    name = session['name']
+    return render_template('checklist-Q12.html', name=name, title=reduceURL(name))
+
+@app.route('/checklist/end')
+def end():
+    return render_template('checklist-end.html')
